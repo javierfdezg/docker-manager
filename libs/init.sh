7726f8844b0301d60125c329a62866fe18f5050c
Javier Fernandez <javier.fdez@gmail.com>
2016-04-18 18:06:26 +0200

    Initial commit

---
+++ libs/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+. libs/utils.sh
+
+configFile='config.json'
+
+declare -a supportedComponents=(db webserver)
+
+init() {
+  if ask "Initialize project?" Y; then
+    if [ -f "${configFile}" ]; then
+      if ask "Config file (${configFile}) is going to be overwritten. Continue?" N; then
+        exit
+      fi
+    fi
+    
+    erase ${configFile};  
+
+    add_variable ${configFile} "string" "projectName"
+    add_variable ${configFile} "string" "projectAddonsDir"
+    add_variable ${configFile} "object" "image"
+    add_variable ${configFile} "object" "container"
+    add_variable ${configFile} "array"  "container.components"
+
+  fi
+}
+
+export -f init
