c000e41e001294fdbc1064a623b07f30aa99f800
Javier Fernandez <javier.fdez@gmail.com>
2016-04-19 15:20:47 +0200

    WIP

--- libs/container.sh
+++ libs/container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Init related functions
+# Container related functions
+
+# Start of not exported functions
 
 add_object_to_array() {
 
@@ -14,6 +16,22 @@ add_object_to_array() {
 
 }
 
+container_help() {
+  echo "$(basename "$0") container {subcommand} [--help|-h]"
+  echo 
+  echo "Available subcommands:"
+  echo
+  echo "create"
+  echo "start"
+  echo "stop"
+  echo "delete"
+  echo
+}
+
+# End of not exported functions
+
+# Start of exported functions
+
 container_add_objects_to_array() {
 	if ask "Add $1?" N; then
 		add_object_to_array $2 $3 $4
@@ -23,12 +41,33 @@ container_add_objects_to_array() {
 	fi
 }
 
-export -f container_add_objects_to_array
-
-# End of init related functions
-
-container_help() {
-	echo "Container help"
+prepare_container() {
+  case $1 in
+    delete)
+      shift
+      prepare_delete_container $@
+      ;;
+    create)
+      shift
+      # TODO
+      prepare_create_container $@
+      ;;
+    start)
+      shift
+      # TODO
+      prepare_start_container $@
+      ;;
+    stop)
+      shift
+      # TODO
+      prepare_stop_container $@
+      ;;
+    *|--help|-h)
+      image_help
+      ;;
+  esac
 }
 
-export -f container_help
+# End of exported functions
+
+export -f container_add_objects_to_array prepare_container
---
+++ libs/image.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Image related functions
+
+# Not exported functions
+
+image_help() {
+  echo "$(basename "$0") image {subcommand} [--help|-h]"
+  echo 
+  echo "Available subcommands:"
+  echo
+  echo "generate"
+  echo "push"
+  echo "delete"
+  echo
+}
+
+
+# End of not exported functions
+
+# Exported functions
+
+prepare_image() {
+  case $1 in
+    delete)
+      shift
+      prepare_delete_image $@
+      ;;
+    generate)
+      shift
+      # TODO
+      prepare_generate_image $@
+      ;;
+    push)
+      shift
+      # TODO
+      prepare_push_image $@
+      ;;
+    *|--help|-h)
+      image_help
+      ;;
+  esac
+}
+
+export -f prepare_image
+
+# End of exported functions
